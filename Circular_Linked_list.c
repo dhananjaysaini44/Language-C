57e2bc5e6d34058e7653cb9c5a8ed69879410138
dhananjaysaini44 <dhananjaysaini4507@gmail.com>
2024-10-23 16:13:33 +0530

    Update Circular_Linked_list.c
    23/10/2024

--- Circular_Linked_list.c
+++ Circular_Linked_list.c
@@ -1,4 +1,3 @@
-// Circular Linked List
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,51 +7,57 @@ typedef struct node
     struct node *next;
 } Node;
 
-Node *head, *tail;
+Node *head = NULL, *tail = NULL;
 
 void newNode(int val)
 {
     Node *p = (Node *)malloc(sizeof(Node));
-
     if (p == NULL)
     {
-        printf("Memory allocation falied.\n");
+        printf("Memory allocation failed.\n");
         exit(1);
+    }
+    p->data = val;
+    p->next = NULL;
+
+    if (head == NULL)
+    {
+        head = p;
+        tail = p;
+        tail->next = head;
     }
     else
     {
-        p->data = val;
-        p->next = NULL;
-
-        if (head == NULL)
-        {
-            head = p;
-            tail = p;
-            tail->next = head;
-        }
-        else
-        {
-            tail->next = p;
-            tail = p;
-            tail->next = head;
-        }
+        tail->next = p;
+        tail = p;
+        tail->next = head;
     }
 }
 
 void insert(int val1, int loc)
 {
+    if (head == NULL && loc != 1)
+    {
+        printf("Invalid location! The list is empty, you can only insert at position 1.\n");
+        return;
+    }
+
+    Node *p = (Node *)malloc(sizeof(Node));
+    if (p == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    p->data = val1;
+
     if (loc == 1)
     {
-        Node *p = (Node *)malloc(sizeof(Node));
-        p->data = val1;
         p->next = head;
         tail->next = p;
         head = p;
     }
     else
     {
-        Node *p = (Node *)malloc(sizeof(Node));
-        p->data = val1;
         Node *q = head;
         for (int i = 1; i <= loc - 2; i++)
         {
@@ -74,14 +79,15 @@ void insert(int val1, int loc)
 
 int deletebydata(int val1)
 {
-    int item;
     if (head == NULL)
     {
         printf("List is empty!\n");
-        exit(1);
+        return -1;
     }
 
-    Node *q = head, *r;
+    int item;
+    Node *q = head, *r = NULL;
+
     if (head->data == val1)
     {
         if (head == tail)
@@ -90,17 +96,16 @@ int deletebydata(int val1)
             free(head);
             head = NULL;
             tail = NULL;
-            return item;
         }
         else
         {
-            q = head;
             item = head->data;
             tail->next = head->next;
+            Node *temp = head;
             head = head->next;
-            free(q);
-            return item;
+            free(temp);
         }
+        return item;
     }
 
     do
@@ -111,8 +116,8 @@ int deletebydata(int val1)
 
     if (q == head)
     {
-        printf("Item not found\n");
-        return 0;
+        printf("Item not found.\n");
+        return -1;
     }
 
     if (q == tail)
@@ -120,22 +125,32 @@ int deletebydata(int val1)
         r->next = head;
         tail = r;
         item = q->data;
-    }
-    else
-    {
-        r->next = q->next;
-        item = q->data;
+        free(q);
+        return item;
     }
 
+    r->next = q->next;
+    item = q->data;
     free(q);
     return item;
 }
 
-int deletebylocation(int loc)
+int deletebylocation(int loc, int count)
 {
+    if (head == NULL)
+    {
+        printf("List is empty!\n");
+        return -1;
+    }
+
+    if (loc < 1 || loc > count)
+    {
+        printf("Invalid location!\n");
+        return -1;
+    }
+
     int item;
-    Node *q = head;
-    Node *r;
+    Node *q = head, *r = NULL;
 
     if (loc == 1)
     {
@@ -148,58 +163,62 @@ int deletebylocation(int loc)
         }
         else
         {
-            Node *q = head;
+            Node *temp = head;
             tail->next = head->next;
             item = head->data;
             head = head->next;
-            free(q);
+            free(temp);
         }
         return item;
+    }
+
+    for (int i = 1; i < loc - 1; i++)
+    {
+        q = q->next;
+    }
+    r = q->next;
+
+    if (r == tail)
+    {
+        q->next = head;
+        tail = q;
+        item = r->data;
     }
     else
     {
-        for (int i = 1; i <= loc - 2; i++)
-        {
-            q = q->next;
-        }
-        r = q->next;
-        item = r->data;
         q->next = r->next;
-        free(r);
-        return item;
+        item = r->data;
     }
+
+    free(r);
+    return item;
 }
 
 void print()
 {
-    Node *p;
-    p = head;
+    if (head == NULL)
+    {
+        printf("No DATA found.\n");
+        return;
+    }
 
-    if (p == NULL)
+    Node *p = head;
+    printf("The data in the linked list is: ");
+    do
     {
-        printf("No DATA Found\n");
-        exit(1);
-    }
-    else
-    {
-        printf("The data in linked list is: ");
-        do
-        {
-            printf("%d ", p->data);
-            p = p->next;
-        } while (p != head);
-        printf("\n");
-    }
+        printf("%d ", p->data);
+        p = p->next;
+    } while (p != head);
+    printf("\n");
 }
 
 int main()
 {
-    int n;
+    int n, count = 0;
     printf("Enter the number of nodes to be created: ");
     scanf("%d", &n);
 
-    int i, count = 0;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         int val;
         printf("Enter the data in new node: ");
@@ -210,16 +229,13 @@ int main()
 
     print();
 
-    int t;
-    printf("Enter the number of test casses: ");
-    scanf("%d", &t);
-    while (t--)
+    int operation = 0;
+    do
     {
-        printf("1. Insert\n2. Delete\nEnter the operation to be performed: ");
-        int n;
-        scanf("%d", &n);
-        int val1, n1, loc, item;
-        switch (n)
+        printf("1. Insert\n2. Delete\n3. Exit\nEnter the operation to be performed: ");
+        scanf("%d", &operation);
+        int val1, loc, item;
+        switch (operation)
         {
         case 1:
             printf("Enter data in new node: ");
@@ -239,16 +255,20 @@ int main()
             break;
         case 2:
             printf("1. Delete by data\n2. Delete by location\nEnter the sub-operation to be performed: ");
-            scanf("%d", &n1);
-            switch (n1)
+            int delOp;
+            scanf("%d", &delOp);
+            switch (delOp)
             {
             case 1:
                 printf("Enter the data to be deleted: ");
                 scanf("%d", &val1);
                 item = deletebydata(val1);
-                count--;
-                printf("Item deleted from the list: %d\n", item);
-                print();
+                if (item != -1)
+                {
+                    count--;
+                    printf("Item deleted from the list: %d\n", item);
+                    print();
+                }
                 break;
             case 2:
                 printf("Enter the location of the node to be deleted: ");
@@ -259,17 +279,37 @@ int main()
                 }
                 else
                 {
-                    item = deletebylocation(loc);
-                    count--;
-                    printf("Item deleted from the list: %d\n", item);
-                    print();
+                    item = deletebylocation(loc, count);
+                    if (item != -1)
+                    {
+                        count--;
+                        printf("Item deleted from the list: %d\n", item);
+                        print();
+                    }
                 }
                 break;
             default:
                 printf("INVALID INPUT!\n");
                 break;
             }
+            break;
+        case 3:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("INVALID INPUT!\n");
+            break;
         }
+    } while (operation != 3);
+
+    while (head != NULL)
+    {
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        if (head == tail->next)
+            break;
     }
+
     return 0;
 }
